85501a1078d85786df5cabc4fa4776c6378c2e49
NicholasPaparusso <nicopaparusso@gmail.com>
2022-12-22 17:31:37 +0100

    [add] Header

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/js/app.js')
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=El+Messiri&display=swap" rel="stylesheet">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.css' integrity='sha512-HHsOC+h3najWR7OKiGZtfhFIEzg5VRIPde0kB0bG2QRidTQqf+sbfcxCTB16AcFB93xMjnBIKE29/MjdzXE+qw==' crossorigin='anonymous'/>
+    <title>laravel-dc</title>
+</head>
+<body>
+
+    @include('partials.header')
+
+    @include('partials.footer')
+
+
+</body>
+</html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,31 @@
+<header>
+
+    <div class="topbar">
+       <div class="container">
+            <a href="#">DC POWER&#8480; VISA&REG;</a> <a href="#">ADDITIONAL DC SITES <i class="fa-solid fa-chevron-down"></i></a>
+       </div>
+    </div>
+    <div class="container">
+
+            <div class="logo">
+                <img src="{{Vite::asset('resources/img/dc-logo.png')}}" alt="">
+            </div>
+
+            <div class="nav">
+                <ul>
+                    <li><a href="#">characters</a></li>
+                    <li><a href="#">comics</a></li>
+                    <li><a href="#">movies</a></li>
+                    <li><a href="#">tv</a></li>
+                    <li><a href="#">games</a></li>
+                    <li><a href="#">collectibles</a></li>
+                    <li><a href="#">videos</a></li>
+                    <li><a href="#">fans'</a></li>
+                    <li><a href="#">news</a></li>
+                    <li><a href="#">shop</a></li>
+                </ul>
+            </div>
+
+
+      </div>
+</header>
