097ef8c84cea07d3c3790e0c0125655940cc2360
NicholasPaparusso <nicopaparusso@gmail.com>
2022-12-22 19:18:38 +0100

    [add] card layout without content

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -3,12 +3,191 @@
 @section('content')
 
 <div class="jumbo">
+    <div class="btn">
+        <h2>current series</h2>
+    </div>
+</div>
+
+<div class="comics">
+
+    <div class="container">
+
+        <div class="card">
+            <div class="image">
+              <img :src="image" alt="">
+              <div class="info-hidden">
+                <p>Prezzo: </p>
+                <p>Tipologia: </p>
+              </div>
+            </div>
+
+            <div class="info">
+              <h4></h4>
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="image">
+              <img :src="image" alt="">
+              <div class="info-hidden">
+                <p>Prezzo: </p>
+                <p>Tipologia: </p>
+              </div>
+            </div>
+
+            <div class="info">
+              <h4></h4>
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="image">
+              <img :src="image" alt="">
+              <div class="info-hidden">
+                <p>Prezzo: </p>
+                <p>Tipologia: </p>
+              </div>
+            </div>
+
+            <div class="info">
+              <h4></h4>
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="image">
+              <img :src="image" alt="">
+              <div class="info-hidden">
+                <p>Prezzo: </p>
+                <p>Tipologia: </p>
+              </div>
+            </div>
+
+            <div class="info">
+              <h4></h4>
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="image">
+              <img :src="image" alt="">
+              <div class="info-hidden">
+                <p>Prezzo: </p>
+                <p>Tipologia: </p>
+              </div>
+            </div>
+
+            <div class="info">
+              <h4></h4>
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="image">
+              <img :src="image" alt="">
+              <div class="info-hidden">
+                <p>Prezzo: </p>
+                <p>Tipologia: </p>
+              </div>
+            </div>
+
+            <div class="info">
+              <h4></h4>
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="image">
+              <img :src="image" alt="">
+              <div class="info-hidden">
+                <p>Prezzo: </p>
+                <p>Tipologia: </p>
+              </div>
+            </div>
+
+            <div class="info">
+              <h4></h4>
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="image">
+              <img :src="image" alt="">
+              <div class="info-hidden">
+                <p>Prezzo: </p>
+                <p>Tipologia: </p>
+              </div>
+            </div>
+
+            <div class="info">
+              <h4></h4>
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="image">
+              <img :src="image" alt="">
+              <div class="info-hidden">
+                <p>Prezzo: </p>
+                <p>Tipologia: </p>
+              </div>
+            </div>
+
+            <div class="info">
+              <h4></h4>
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="image">
+              <img :src="image" alt="">
+              <div class="info-hidden">
+                <p>Prezzo: </p>
+                <p>Tipologia: </p>
+              </div>
+            </div>
+
+            <div class="info">
+              <h4></h4>
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="image">
+              <img :src="image" alt="">
+              <div class="info-hidden">
+                <p>Prezzo: </p>
+                <p>Tipologia: </p>
+              </div>
+            </div>
+
+            <div class="info">
+              <h4></h4>
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="image">
+              <img :src="image" alt="">
+              <div class="info-hidden">
+                <p>Prezzo: </p>
+                <p>Tipologia: </p>
+              </div>
+            </div>
+
+            <div class="info">
+              <h4></h4>
+            </div>
+          </div>
+
+        <div class="btn">
+          <h5>load more</h5>
+        </div>
+
+    </div>
 
 </div>
 
-<div class="test">
-    <h1>
-        comics
-    </h1>
-</div>
+
+
 @endsection
